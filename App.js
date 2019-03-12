2764e8e9d8afe3b61943124ab70914e3806d331d
Raiff <rmello@lenovo.com>
2019-03-12 08:44:40 -0300

    styling and layout ok

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
+import History from './components/History'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
@@ -10,8 +11,8 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View>
-          <AddEntry />
+        <View style={{flex: 1}}>
+          <History />
         </View>
       </Provider>
     );
--- components/TextButton.js
+++ components/TextButton.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import {Text, TouchacbleOpacity} from 'react-native'
+import {Text, TouchacbleOpacity, StyleSheet} from 'react-native'
+import { purple } from '../utils/colors'
 
-export default function TextButton ({children, onPress}){
+
+export default function TextButton ({children, onPress, style = {}}){
   return (
     <TouchacbleOpacity onPress={onPress}>
-      <Text>{children}</Text>
+      <Text style={[styles.reset, style]}>{children}</Text>
     </TouchacbleOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  reset: {
+    textAlign: 'center',
+    color: purple,
+  }
+}) 
